824b9d758af947bc5f9dbe641bd5c355a92a83b3
Ben Moser <benjamin.moser@hs-augsburg.de>
2020-12-21 17:19:16 +0100

    added auftrag information to tasks on tasklist

--- src/components/task/TaskItem.js
+++ src/components/task/TaskItem.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import Moment from 'react-moment';
 
 import { useAuftrag, getSelectedAuftrag } from '../../context/auftrag/auftragState';
 
@@ -13,7 +14,7 @@ const TaskItem = ({ task }) => {
 
 	return (
 		<Fragment>
-			<div className='col s12 m3'>
+			<div className='col s12 m6 l3'>
 				<a href='#' onClick={loadForm}>
 					<div className='card-panel white'>
 						<span className='black-text'>
@@ -22,12 +23,25 @@ const TaskItem = ({ task }) => {
 						<div className='divider yellow darken-4'></div>
 						<br />
 						<span className='black-text'>
-							<b>Created at: </b>
-							{task.created}
+							Auftragname:
+							<br />
+							<b>{task.auftragName}</b>
 						</span>
 						<br />
 						<span className='black-text'>
-							<b>ProzessID: </b> {task.processInstanceId}
+							Bauvorhaben:
+							<br />
+							<b>{task.bauvorhaben}</b>
+						</span>
+						<br />
+						<span className='black-text'>
+							Erstellt am:
+							<br />
+							<b>
+								<Moment format='Do MMMM YYYY'>{task.created}</Moment>
+								<br />
+								<Moment format='k:mm:ss'>{task.created}</Moment>
+							</b>
 						</span>
 					</div>
 				</a>
--- src/context/task/taskState.js
+++ src/context/task/taskState.js
@@ -14,13 +14,59 @@ export const useTask = () => {
 //get tasks
 export const getTasks = async (dispatch) => {
 	try {
-		const resTasks = await axios.get('http://localhost:8088/engine-rest/task');
+		//get tasks
+		let resTasks = await axios.get('http://localhost:8088/engine-rest/task');
+		resTasks = resTasks.data;
+		//console.log('resTasks:', resTasks);
 
-		//ToDo: add infos about the auftrag for the task to display it on the taskboard
+		/******/
+		/** add infos about the auftrag to the task to display auftragname und bauvorhaben on the taskboard */
+		/******/
+
+		//get instances
+		let resInstances = await axios.get('http://localhost:8088/engine-rest/process-instance');
+		resInstances = resInstances.data;
+		//console.log('resInstances:', resInstances);
+
+		//get only processInstanceId and businessKey
+		let processInstances = resInstances.map((instance) => {
+			return { processInstanceId: instance.id, businessKey: instance.businessKey };
+		});
+		//console.log('processInstances:', processInstances);
+
+		//get aufträge
+		let resAuftraege = await axios.get('http://localhost:8088//restapi/auftrag');
+		resAuftraege = resAuftraege.data;
+		//console.log('resAuftraege:', resAuftraege);
+
+		//rename property name to auftragName
+		resAuftraege = resAuftraege.map((auftrag) => {
+			return { ...auftrag, auftragName: auftrag.name };
+		});
+
+		//merge instances and auftraege
+		let instancesAndAuftraege = processInstances.map((instance) => {
+			for (let i = 0; i < resAuftraege.length; i++) {
+				if (instance.businessKey === resAuftraege[i].id) {
+					return Object.assign(instance, resAuftraege[i]);
+				}
+			}
+		});
+		//console.log('instancesAndAuftraege', instancesAndAuftraege);
+
+		//merge instancesAndAuftraege and tasks
+		let tasksAndAuftraege = instancesAndAuftraege.map((auftrag) => {
+			for (let i = 0; i < resTasks.length; i++) {
+				if (auftrag.processInstanceId === resTasks[i].processInstanceId) {
+					return Object.assign(auftrag, resTasks[i]);
+				}
+			}
+		});
+		console.log('tasksAndAuftraege', tasksAndAuftraege);
 
 		dispatch({
 			type: GET_TASKS,
-			payload: resTasks.data
+			payload: tasksAndAuftraege
 		});
 	} catch (error) {
 		console.log('Error:', error);
